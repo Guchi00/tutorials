cbcf8e8648c66ae8f4399e8b45a82e3acc78360f
Emmanuel Ihemegbulam <emmanuelroic@gmail.com>
2021-06-29 19:32:04 +0100

    added datatypes and homework

---
+++ javascript/variables/dataTypes.js
@@ -0,0 +1,43 @@
+
+// Data types in Javascript
+
+// 1. String
+const  favMovie = "Mad Mad Fury Road";
+const favFood = 'Rice & Chicken';
+
+// 2. Number
+const macbookCost = 2000;
+const height = 5.5;
+
+// 3. Boolean
+const isLeapYear = false;
+const monthIsJune = true;
+
+// 4. Object
+const max = {
+  firstName: 'Max',
+  lastName: 'Rockatansky',
+  weapons: ['Shotgun', 'machete', 'grenade']
+};
+
+const myCar = {
+  make: 'Mercedes',
+  model: 'G Class',
+  year: '2021',
+  isLatestModel: true,
+  noOfWheels: 6
+};
+
+// 5. Array
+const todayMenu = [
+  'Rice & Stew',
+  'Beans & Plantain',
+  'Nkwobi & Goat Meat'
+];
+
+const todaySale = [
+  { item: 'Rice & Stew', quantity: 20 },
+  { item: 'Beans & Plantain', quantity: 12 },
+  { item: 'Nkwobi & Goat Meat', quantity: 35 }
+];
+
