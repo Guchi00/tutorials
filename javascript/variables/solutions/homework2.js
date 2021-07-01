a997e4f0fe50b43dbbbdc7588b8d218a15126578
Ugochi Iwuchukwu <iwuchukwuugochij@gmail.com>
2021-07-01 05:24:48 -0700

    completed homework

---
+++ javascript/variables/solutions/homework2.js
@@ -0,0 +1,88 @@
+//Number
+const numb1 = 10;
+const numb2 = 10.5;
+
+let feet;
+feet = 7;
+let height = 9.5;
+
+//String
+const name = 'loveth';
+const phone = "iphone12ProMax";
+
+let movie = "theSunRise";
+let footWear;
+footWear = 'boyfriendShoe';
+
+//Boolean
+const ugochiFirstBornChild = false;
+const emmaLastBornChild = true;
+
+let weAteRiceInTheMorning;
+weAteRiceInTheMorning= true;
+
+let areAllMenDark = false;
+
+//Object
+const girl = {
+    firstName:'ngozi',
+    middleName: 'blessing',
+    age: 17,
+    eyeColor: 'grey',
+    sheIsDarkInComplexion: false,
+    shoes: ['wage', 'heel', 'flate']
+};
+
+const halfOfAYellowSun = {
+    author: "chimamandaNgoziAdichie",
+    publishedYear: "2006",
+    noOfPages: 448,
+    consistOfNigeriaBiafraWar: true,
+    theAuthorYoruba: false,
+    countriesOfCirculation: ["nigeria", "unitedStateOfAmerica", "france", "canada"]
+};
+
+let countryNigeria = {
+    federalTerritory: 'abuja',
+    noStates: 36,
+    localGovernment774: true,
+    state: ['lagos', 'anambra', 'imo', 'ogun']
+};
+
+let house;
+house = {
+    rooms: "eight",
+    color: "whiteAndGrey",
+    storyBuilding: 2,
+    bungalow: false,
+    occupant: ["father", "mother", "children"]
+};
+
+//Array
+const cars = [
+    'bmw', 
+    'volvo', 
+    'toyota',
+    'peugeot'
+];
+const fan = [
+   {modern: "lontor", color: "whiteAndGrey"},
+   {modern: "lg", color: "darkBlue"},
+   {modern: "swiss", color: "black"}
+];
+
+let  listOfPrimaryColor;
+listOfPrimaryColor = [
+    "red",
+    "yellow",
+    "blue"
+];
+let myWardrobeItems = [
+    {gucci: 'boyfriendShoe', color: 'black', size: '38'},
+    {balenciaga: 'bag', color: 'red', size: '3.5inches'},
+    {chanel: 'sunglasses', color: 'white', size: '5.5inches'}
+];
+
+
+
+
