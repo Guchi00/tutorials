b798eaa704a2f1111eb9faa5405921b35674220e
Emmanuel Ihemegbulam <emmanuelroic@gmail.com>
2021-07-03 18:38:38 +0100

    create operators

---
+++ javascript/operators/math.js
@@ -0,0 +1,28 @@
+
+// Mathematical Operators enable you to perform math operations on variables
+// Addition: +
+// Subtraction: -
+// Multiplication: *
+// Division: /
+
+// Examples
+const a = 5;
+const b = 8;
+
+const sum = a + b;
+const sub = a - b;
+const mul = a * b;
+const quotient = a / b;
+
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(quotient);
+
+/*
+  Create two variables
+  Perform arithmetic operations on them
+  (addition, subtraction, multiplication, division)
+*/
+
