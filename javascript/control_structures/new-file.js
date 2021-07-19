5b47d2ef03e0dde5ca312a8ab5e5ec0d9b3f83e1
Ugochi Iwuchukwu <iwuchukwuugochij@gmail.com>
2021-07-19 04:19:20 -0700

    want to switch to a new branch

---
+++ javascript/control_structures/new-file.js
@@ -0,0 +1,25 @@
+const listOfNames = [
+    'Ada',
+    'Obi',
+    'Nkechi',
+    'Amaka',
+    'Ego'
+];
+let name = listOfNames[0];
+// console.log(listOfNames[0]);
+
+// console.log(listOfNames[1]);
+
+// console.log(listOfNames[2]);
+
+// console.log(listOfNames[3]);
+
+// console.log(listOfNames[4]);
+
+for (let a = 0; a < 5; a++) {
+    console.log(listOfNames[a]);
+}
+
+for (let a = 0; a < 5; a++) {
+    console.log(listOfNames[a]);
+}
